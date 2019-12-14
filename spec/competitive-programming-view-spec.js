c26a881751d25bcbc937c33fa281c4ce347065bc
Hassan El Desouky <hassaneldesouky@icloud.com>
2019-12-14 05:27:50 +0200

    Initial commit

---
+++ spec/competitive-programming-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CompetitiveProgrammingView from '../lib/competitive-programming-view';
+
+describe('CompetitiveProgrammingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
